16cafda2453815ed09e78d31aec46bcff8304a65
qwerty_psv <psvishnu.it13@gmail.com>
2018-07-27 21:24:41 +0530

    Lesson 1 : Naive bayes. lso modified email_preprocess to avoid out of memory

--- machine-learning-project/README.md
+++ machine-learning-project/README.md
@@ -2,3 +2,7 @@ ud120-projects
 ==============
 
 Starter project code for students taking Udacity ud120
+
+### Lesson 1 : Naive Bayes
+
+![Output](../images/NaiveBayes.PNG)
--- machine-learning-project/naive_bayes/nb_author_id.py
+++ machine-learning-project/naive_bayes/nb_author_id.py
@@ -12,9 +12,9 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("/Users/Focus/Documents/GitHub/udacity/machine-learning-project/tools")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,8 +26,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+print clf.score(features_test, labels_test)
 #########################################################
 
 
