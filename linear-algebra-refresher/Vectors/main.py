9173e6a120a51802d8ee7251d1eefb093c4cafc8
p-s-vishnu <psvishnu.it13@gmail.com>
2018-10-15 08:36:29 +0530

    Coding dot product and angle

--- linear-algebra-refresher/Vectors/main.py
+++ linear-algebra-refresher/Vectors/main.py
@@ -3,29 +3,48 @@ from vector import Vector
 #test equal
 test_vec		= Vector((1,2,3))
 test_vec1		= Vector((1,2,3))
-print('Check if equal : ', test_vec == test_vec1)
+# print('Check if equal : ', test_vec == test_vec1)
 
 # Add operation
 a = Vector((8.218, -9.341))
 b = Vector((-1.129, 2.111))
-print('Sum of 2 Vec :', a.add_vector(b))
+# print('Sum of 2 Vec :', a.add_vector(b))
 
 # Subtract
 c = Vector((7.119, 8.215))
 d = Vector((-8.223, 0.878))
-print('Diff of 2 Vec :', c.sub_vector(d))
+# print('Diff of 2 Vec :', c.sub_vector(d))
 
 # Scalar mul
-print('Scalar mul :', d.mul_vector(2))
+# print('Scalar mul :', d.mul_vector(2))
 
 # magnitude
 e = Vector((-0.221, 7.437))
 f = Vector((8.813, -1.331, -6.247))
-print(e,e.magnitude())
-print(f,f.magnitude())
+# print(e,e.magnitude())
+# print(f,f.magnitude())
 
 # normalization
 g = Vector((5.581, -2.136)) 
 h =	Vector((1.996, 3.108, -4.554))
-print(g,g.normalization())
-print(h.normalization())
+# print(g,g.normalization())
+# print(h.normalization())
+
+# Dot product (Inner product)
+v1 = Vector((7.887, 4.138))
+w1 = Vector((-8.802, 6.776))
+# print(v1.dot_product(w1))
+
+v2 = Vector((-5.955, -4.904, -1.874))
+w2 = Vector((-4.496, -8.755, 7.103))
+# print(v2.dot_product(w2))
+
+# Angle in radians
+v = Vector((3.183, -7.627))
+w = Vector((-2.668, 5.319))
+print(v.angle_in_rad(w))
+
+# Angle in degree
+v = Vector((7.35, 0.221, 5.188))
+w = Vector((2.751, 8.259, 3.985))
+print(v.angle_in_degree(w))
--- linear-algebra-refresher/Vectors/vector.py
+++ linear-algebra-refresher/Vectors/vector.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, acos, pi
 
 class Vector(object):
 
@@ -38,3 +38,20 @@ class Vector(object):
 	def	normalization(self):
 		m = self.magnitude()
 		return self.mul_vector(1/m)
+
+	def dot_product(self, new_vector):
+		if self.coordinates == 0 or new_vector.coordinates == 0:
+			return 0
+		return round(sum([i*j for i,j in zip(self.coordinates, new_vector.coordinates)]), 3)
+
+	def angle_in_rad(self, new_vector):
+		dot_value	= self.dot_product(new_vector)
+		mag_v		= self.magnitude()
+		mag_w		= new_vector.magnitude()
+		# arc cosine - acos
+		return acos(dot_value / (mag_v * mag_w))
+
+	def angle_in_degree(self, new_vector):
+		angle_rad	= self.angle_in_rad(new_vector)
+		scalar		= 180 / pi
+		return scalar * angle_rad
